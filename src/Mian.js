f3cf9c3d1702498cc992ccaacc0047123d9524f5
haseebzahid1 <mmianhaseeb444@gmail.com>
2022-05-13 19:58:34 +0500

    skills section responsive added

--- src/Mian.js
+++ src/Mian.js
@@ -12,9 +12,9 @@ const Mian = () => {
       <Header />
       <Greeting />
       <Skills />
-      <SkillsProgress />
+      {/* <SkillsProgress />
       <WorkExperience />
-      <Achievement />
+      <Achievement /> */}
     </>
   )
 }
--- src/components/skillProgress/SkillsProgress.js
+++ src/components/skillProgress/SkillsProgress.js
@@ -3,28 +3,34 @@ import { techStack } from '../../portfolio'
 import '../../components/skillProgress/SkillsProgress.css'
 
 const SkillsProgress = () => {
-  return (
-    <div className='container'>
-     <div className='skills-bar'>
-     <h1 className="skills-heading">Proficiency</h1>
-     {techStack.experience.map((exp)=>{
+    return (
+        <div className='container'>
+            <div className='progress-container'>
+                <div className='progress-content'>
+                <h1 className="progress-heading">Proficiency</h1>
+                    {techStack.experience.map((exp) => {
 
-         const progressStyle = {
-            width: exp.progressPercentage
-        };
-        
-        return (
-        <div className="skill">
-            <p className='skills-p'>{exp.Stack}</p>
-            <div className="meter">
-                <span style={progressStyle}></span>
+                        const progressStyle = {
+                            width: exp.progressPercentage
+                        };
+
+                        return (
+                            <div className="skill">
+                                <p className='skills-p'>{exp.Stack}</p>
+                                <div className="meter">
+                                    <span style={progressStyle}></span>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+
+                <div className='progress-img'>
+                    <img src="img/skill.svg" alt="" />
+                </div>
             </div>
         </div>
-        );
-     })}
-     </div>
-    </div>
-  )
+    )
 }
 
 export default SkillsProgress
